6cf94625a0641908f0ef27471e0d95fb504dea50
Mateus Motta <56051761+Motta21@users.noreply.github.com>
2025-11-24 21:37:27 -0300

    Update historico.php

--- projeto/historico.php
+++ projeto/historico.php
@@ -5,7 +5,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once 'db_conexao.php'; 
+require_once 'php/db_conection.php'; 
 
 // 2. CONFIGURAÇÃO E CONSULTA À VIEW
 $view_dados = "view_estacao";
