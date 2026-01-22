88ffdc2113bc4d1eddea6ba8a5b517d30c7e551f
Mateus Motta <56051761+Motta21@users.noreply.github.com>
2026-01-22 20:11:41 -0300

    Update graficos.js

--- projeto/graficos.js
+++ projeto/graficos.js
@@ -60,9 +60,9 @@ function atualizarGraficos(dados) {
     criarOuAtualizar("graficoOrvalho", labels, orvalho, "Ponto de Orvalho (°C)", cores.orvalho);
     criarOuAtualizar("graficoBat", labels, bat, "Bateria (V)", cores.bat);
     criarOuAtualizar("graficoTemUmiPto", labels, [
-    { label: "Temp", data: temperatura, borderColor: cores.temperatura, yAxisID: 'y', tension: 0.4 },
-    { label: "Umi", data: umidade, borderColor: cores.umidade, tension: 0.4 },
-    { label: "Orvalho", data: orvalho, borderColor: cores.orvalho, yAxisID: 'y1', tension: 0.4 }
+    { label: "Temp", data: temperatura, borderColor: cores.temperatura, yAxisID: 'y1', tension: 0.4 },
+    { label: "Umi", data: umidade, borderColor: cores.umidade, yAxisID: 'y1', tension: 0.4 },
+    { label: "Orvalho", data: orvalho, borderColor: cores.orvalho, yAxisID: 'y', tension: 0.4 }
 ], "Clima");
 
 
